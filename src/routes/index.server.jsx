4625986a3951f59f69a33ffe7b8f52a2b018e70d
sagarchandresha <sagar.bsquaresoft@gmail.com>
2022-07-27 19:05:13 +0530

    add slider

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import SlickHome from "../components/SlickHome.client";
 
 export default function Home() {
-  return <SlickHome />;
+  return (
+    <Suspense>
+      <SlickHome />
+    </Suspense>
+  );
 }
