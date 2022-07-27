7428530ecb5f951dcf695f97cc41f3300495c7d7
sagarchandresha <sagar.bsquaresoft@gmail.com>
2022-07-27 19:11:02 +0530

    add slider

--- src/components/SlickHome.client.jsx
+++ src/components/SlickHome.client.jsx
@@ -1,6 +1,6 @@
 // The component name must finish with .client.jsx | .client.tsx
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
 
 import React from 'react';
 import Slider from 'react-slick';
