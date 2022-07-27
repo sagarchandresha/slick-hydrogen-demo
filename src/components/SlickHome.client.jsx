8eee17e280259cc4c05ed81560e6977c563feb24
sagarchandresha <sagar.bsquaresoft@gmail.com>
2022-07-27 18:02:25 +0530

    add slider

--- src/components/SlickHome.client.jsx
+++ src/components/SlickHome.client.jsx
@@ -1,6 +1,6 @@
 import { Link } from "@shopify/hydrogen";
 import React from "react";
-import Slider from "react-slick";
+// import Slider from "react-slick";
 // Import css files
 // import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
